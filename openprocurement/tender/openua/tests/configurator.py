c6eff562689f9a3edc7668c3b8ede7d23f205b9b
MotsoVitalii <vitalii.motso@gmail.com>
2017-10-03 11:20:33 +0300

    add a test of configurator value

--- openprocurement/tender/openua/tests/configurator.py
+++ openprocurement/tender/openua/tests/configurator.py
@@ -1,17 +1,19 @@
 import unittest
 from openprocurement.tender.core.tests.configurator import ConfiguratorTestMixin
 from openprocurement.tender.openua.adapters import TenderAboveThresholdUAConfigurator
+from openprocurement.tender.openua.models import Tender
 
 
 class ConfiguratorTest(unittest.TestCase, ConfiguratorTestMixin):
     configurator_class = TenderAboveThresholdUAConfigurator
     reverse_awarding_criteria = False
     awarding_criteria_key = 'not yet implemented'
+    configurator_model = Tender
 
 
 def suite():
-    current_suite = unittest.TestSuite()
-    current_suite.addTest(unittest.makeSuite(ConfiguratorTest))
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ConfiguratorTest))
     return suite
 
 
